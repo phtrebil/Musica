a485c026aab968a50157da402ffdd781f6e8305d
phtrebil <phptrebil@gmail.com>
2023-04-07 20:20:08 -0300

    criando primeiro layout

--- app/src/main/java/com/example/musica/MainActivity.kt
+++ app/src/main/java/com/example/musica/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.musica
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        solicitacaoDePermissoes()
+
+    }
+
+    fun solicitacaoDePermissoes(){
+        Dexter.withContext(this)
+            .withPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                }
+            }).check()
     }
 }
