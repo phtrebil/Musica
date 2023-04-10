5db4b5e34ac86fef540c5870f0728a141e7cbeda
phtrebil <phptrebil@gmail.com>
2023-04-10 19:33:39 -0300

    criando adapter

---
+++ app/src/main/java/com/example/musica/MusicaAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.musica
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MusicaAdapter : RecyclerView.Adapter<MusicaAdapter.MusicaViewHolder>() {
+
+
+    inner class MusicaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicaViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MusicaViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
